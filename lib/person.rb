b4faf38e86688a9c1e65eef2b901e5802c66ac50
James Sapp <jameseisapp@gmail.com>
2020-12-10 15:25:04 -0600

    commit tree 493a4d49951cb21f7502009a1ee6b99fced4a02a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    # attr_accessor :bank_account, :happiness, :hygiene
+    # attr_reader :name 
+
+    def initialize(name)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def name
+        @name
+    end
+    
+    def bank_account #setter
+        @bank_account = 25
+        # @bank_account += 1 
+    end
+
+    def happiness
+        @happiness = 8
+    end
+
+    def hygiene
+        @hygiene = 8
+    end
+
+
+end
+
+#binding.pry 
